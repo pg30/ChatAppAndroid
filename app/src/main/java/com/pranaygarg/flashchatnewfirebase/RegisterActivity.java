a51819df20c8834e0713676839622ffbb9738ec7
Pranay Garg <pranaygarg001@gmail.com>
2019-09-14 00:25:44 +0530

    Registration Module Completed

--- app/src/main/java/com/pranaygarg/flashchatnewfirebase/RegisterActivity.java
+++ app/src/main/java/com/pranaygarg/flashchatnewfirebase/RegisterActivity.java
@@ -1,14 +1,24 @@
 package com.pranaygarg.flashchatnewfirebase;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class RegisterActivity extends AppCompatActivity {
@@ -25,7 +35,7 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText mConfirmPasswordView;
 
     // Firebase instance variables
-
+    FirebaseAuth mAuth;
 
 
     @Override
@@ -43,6 +53,7 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == R.integer.register_form_finished || id == EditorInfo.IME_NULL) {
+                    Log.d("Chat","query method");
                     attemptRegistration();
                     return true;
                 }
@@ -51,8 +62,7 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
         // TODO: Get hold of an instance of FirebaseAuth
-
-
+        mAuth = FirebaseAuth.getInstance();
     }
 
     // Executed when Sign Up button is pressed.
@@ -97,6 +107,7 @@ public class RegisterActivity extends AppCompatActivity {
             focusView.requestFocus();
         } else {
             // TODO: Call create FirebaseUser() here
+            createFirebaseUser();
 
         }
     }
@@ -108,17 +119,53 @@ public class RegisterActivity extends AppCompatActivity {
 
     private boolean isPasswordValid(String password) {
         //TODO: Add own logic to check for a valid password (minimum 6 characters)
-        return true;
+        String confirmPassword = mConfirmPasswordView.getText().toString();
+        return confirmPassword.equals(password) && password.length()>4;
     }
 
     // TODO: Create a Firebase user
-
+    private void createFirebaseUser()
+    {
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("Chat","createUser onComplete"+task.isSuccessful());
+                if(!task.isSuccessful())
+                {
+                    Log.d("Chat","createUser Failed");
+                    showErrorDialog("Registration attempt failed");
+                }
+                else
+                {
+                    saveDisplayName();
+                    Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                    finish();
+                    startActivity(intent);
+                }
+            }
+        });
+    }
 
     // TODO: Save the display name to Shared Preferences
-
+    private void saveDisplayName()
+    {
+        String displayName = mUsernameView.getText().toString();
+        SharedPreferences prefs = getSharedPreferences(CHAT_PREFS,0);
+        prefs.edit().putString(DISPLAY_NAME_KEY,displayName).apply();
+    }
 
     // TODO: Create an alert dialog to show in case registration failed
-
+    private void showErrorDialog(String message)
+    {
+        new AlertDialog.Builder(this)
+                .setTitle("Oops")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok,null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
 
 
 
